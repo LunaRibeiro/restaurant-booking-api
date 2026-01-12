9d13b1afd1db75a59ccab23baa4358cfce2ffdfa
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2026-01-12 16:57:30 -0300

    feat: add Users entity with basic fields and annotations

---
+++ src/main/java/br/com/restaurant_reservation_api/core/users/domain/entity/Users.java
@@ -0,0 +1,34 @@
+package br.com.restaurant_reservation_api.core.users.domain.entity;
+
+import br.com.restaurant_reservation_api.core.role.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}

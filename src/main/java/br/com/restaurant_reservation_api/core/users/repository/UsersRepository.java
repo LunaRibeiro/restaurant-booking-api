4dd7bf3b7bae150cffc6f7ddf1f57c593814bdeb
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2026-01-12 17:18:26 -0300

    feat: add UsersRepository interface for user data persistence

---
+++ src/main/java/br/com/restaurant_reservation_api/core/users/repository/UsersRepository.java
@@ -0,0 +1,9 @@
+package br.com.restaurant_reservation_api.core.users.repository;
+
+import br.com.restaurant_reservation_api.core.users.domain.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long> {
+}

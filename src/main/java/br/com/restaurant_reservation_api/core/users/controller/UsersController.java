fd2a257f5b5025d120376604a2bfb097a806577a
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2026-01-22 14:26:28 -0300

    refactor: use HttpUtils for URI creation in UsersController

--- src/main/java/br/com/restaurant_reservation_api/core/users/controller/UsersController.java
+++ src/main/java/br/com/restaurant_reservation_api/core/users/controller/UsersController.java
@@ -1,5 +1,6 @@
 package br.com.restaurant_reservation_api.core.users.controller;
 
+import br.com.restaurant_reservation_api.common.utils.HttpUtils;
 import br.com.restaurant_reservation_api.core.users.domain.dto.request.UsersFormDTO;
 import br.com.restaurant_reservation_api.core.users.domain.dto.response.UsersDTO;
 import br.com.restaurant_reservation_api.core.users.domain.entity.Users;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 
@@ -23,20 +24,16 @@ public class UsersController {
     private final UsersService usersService;
 
     @PostMapping
-    public ResponseEntity<UsersDTO> create(@RequestBody @Valid UsersFormDTO usersFormDTO) {
+    public ResponseEntity<UsersDTO> create(@RequestBody @Valid UsersFormDTO usersFormDTO, UriComponentsBuilder uriComponentsBuilder) {
 
         Users users = usersService.generateUsers(usersFormDTO);
         usersService.save(users);
 
         UsersDTO usersDTO = usersService.generateUsersDTO(users);
 
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(users.getId())
-                .toUri();
+        URI uri = HttpUtils.createURI(uriComponentsBuilder, "users", users.getId());
 
-        return ResponseEntity.created(location).body(usersDTO);
+        return ResponseEntity.created(uri).body(usersDTO);
     }
 
 }

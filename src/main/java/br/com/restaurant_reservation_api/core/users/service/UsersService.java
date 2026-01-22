a35a85aaa7ecf8e02f6e7907e00cfe69a0da1507
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2026-01-22 14:47:57 -0300

    feat: add dynamic user filtering and pagination in UsersService

--- src/main/java/br/com/restaurant_reservation_api/core/users/service/UsersService.java
+++ src/main/java/br/com/restaurant_reservation_api/core/users/service/UsersService.java
@@ -1,5 +1,9 @@
 package br.com.restaurant_reservation_api.core.users.service;
 
+import br.com.restaurant_reservation_api.common.specification.SearchCriteria;
+import br.com.restaurant_reservation_api.common.specification.SpecificationHelper;
+import br.com.restaurant_reservation_api.core.role.Role;
+import br.com.restaurant_reservation_api.core.users.domain.dto.request.UsersFilterDTO;
 import br.com.restaurant_reservation_api.core.users.domain.dto.request.UsersFormDTO;
 import br.com.restaurant_reservation_api.core.users.domain.dto.response.UsersDTO;
 import br.com.restaurant_reservation_api.core.users.domain.entity.Users;
@@ -7,8 +11,14 @@ import br.com.restaurant_reservation_api.core.users.mapper.UsersCreateMapper;
 import br.com.restaurant_reservation_api.core.users.mapper.UsersDTOMapper;
 import br.com.restaurant_reservation_api.core.users.mapper.UsersUpdateMapper;
 import br.com.restaurant_reservation_api.core.users.repository.UsersRepository;
+import br.com.restaurant_reservation_api.core.users.specification.UsersSpecification;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -36,4 +46,35 @@ public class UsersService {
         return usersRepository.findById(id).orElse(null);
     }
 
+    public List<Users> list(UsersFilterDTO usersFilterDTO){
+        Specification<Users> userSpecification = generateSpecification(usersFilterDTO);
+        return usersRepository.findAll(userSpecification);
+    }
+
+    public Page<Users> list(UsersFilterDTO usersFilterDTO, Pageable pageable){
+        Specification<Users> userSpecification = generateSpecification(usersFilterDTO);
+        return usersRepository.findAll(userSpecification, pageable);
+    }
+
+    private Specification<Users> generateSpecification(UsersFilterDTO usersFilterDTO){
+        SearchCriteria<String> nameCriteria = SpecificationHelper.generateEqualsCriteria("name", usersFilterDTO.name());
+        SearchCriteria<String> emailCriteria = SpecificationHelper.generateEqualsCriteria("email", usersFilterDTO.email());
+        SearchCriteria<Role> roleCriteria = SpecificationHelper.generateEqualsCriteria("role", usersFilterDTO.role());
+
+        UsersSpecification nameSpecification = new UsersSpecification(nameCriteria);
+        UsersSpecification emailSpecification = new UsersSpecification(emailCriteria);
+        UsersSpecification roleSpecification = new UsersSpecification(roleCriteria);
+
+        return Specification.where(nameSpecification)
+                .and(emailSpecification)
+                .and(roleSpecification);
+    }
+
+    public Page<UsersDTO> generateUsersDTOPage(Page<Users> usersPage, Pageable pageable){
+        return usersPage.map(this::generateUsersDTO);
+    }
+
+    public List<UsersDTO> generateUsersDTOList(List<Users> usersList){
+        return usersList.stream().map(usersDTOMapper::convert).toList();
+    }
 }

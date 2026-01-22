c9f848e590c8476fbb5db4c4cf7fe12ee9330201
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2026-01-22 15:34:49 -0300

    feat: add RestaurantTableRepository interface with JPA support

---
+++ src/main/java/br/com/restaurant_reservation_api/core/restauranttable/entity/RestaurantTableRepository.java
@@ -0,0 +1,10 @@
+package br.com.restaurant_reservation_api.core.restauranttable.entity;
+
+import br.com.restaurant_reservation_api.core.restauranttable.domain.entity.RestaurantTable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantTableRepository extends JpaRepository<RestaurantTable, Long>, JpaSpecificationExecutor<RestaurantTable> {
+}

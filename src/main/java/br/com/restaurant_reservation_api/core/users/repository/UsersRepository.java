7ca8d76c742cc947bf5cab3fb2da9ff98288a944
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2026-01-22 14:41:47 -0300

    feat: extend UsersRepository with JpaSpecificationExecutor for dynamic queries

--- src/main/java/br/com/restaurant_reservation_api/core/users/repository/UsersRepository.java
+++ src/main/java/br/com/restaurant_reservation_api/core/users/repository/UsersRepository.java
@@ -2,8 +2,9 @@ package br.com.restaurant_reservation_api.core.users.repository;
 
 import br.com.restaurant_reservation_api.core.users.domain.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsersRepository extends JpaRepository<Users, Long> {
+public interface UsersRepository extends JpaRepository<Users, Long>, JpaSpecificationExecutor<Users> {
 }

2c007e42141ad267bc594aa34cb3f58142fd0d30
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2026-01-22 14:40:54 -0300

    refactor: update UsersSpecification to use Users entity instead of User

--- src/main/java/br/com/restaurant_reservation_api/core/users/specification/UsersSpecification.java
+++ src/main/java/br/com/restaurant_reservation_api/core/users/specification/UsersSpecification.java
@@ -2,8 +2,8 @@ package br.com.restaurant_reservation_api.core.users.specification;
 
 import br.com.restaurant_reservation_api.common.specification.BaseSpecification;
 import br.com.restaurant_reservation_api.common.specification.SearchCriteria;
-import org.apache.catalina.User;
+import br.com.restaurant_reservation_api.core.users.domain.entity.Users;
 
-public class UsersSpecification extends BaseSpecification<User> {
+public class UsersSpecification extends BaseSpecification<Users> {
     public UsersSpecification(SearchCriteria<?> searchCriteria){ super(searchCriteria); }
 }

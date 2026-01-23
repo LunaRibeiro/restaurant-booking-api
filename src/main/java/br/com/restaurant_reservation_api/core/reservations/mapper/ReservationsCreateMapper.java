fc1995e24e4724d4dab938664bc7308b493e84b1
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2026-01-23 14:39:04 -0300

    feat: add mappers for Reservations creation and DTO handling

---
+++ src/main/java/br/com/restaurant_reservation_api/core/reservations/mapper/ReservationsCreateMapper.java
@@ -0,0 +1,20 @@
+package br.com.restaurant_reservation_api.core.reservations.mapper;
+
+import br.com.restaurant_reservation_api.core.reservations.domain.dto.request.ReservationsFormDTO;
+import br.com.restaurant_reservation_api.core.reservations.domain.entity.Reservations;
+import br.com.restaurant_reservation_api.core.restauranttables.domain.entity.RestaurantTables;
+import br.com.restaurant_reservation_api.core.users.domain.entity.Users;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationsCreateMapper {
+
+    public Reservations convert(ReservationsFormDTO reservationsFormDTO, Users users, RestaurantTables restaurantTables) {
+        Reservations reservations = new Reservations();
+        reservations.setUserId(users);
+        reservations.setRestaurantTableId(restaurantTables);
+        reservations.setReservationDate(reservationsFormDTO.reservationDate());
+
+        return reservations;
+    }
+}
---
+++ src/main/java/br/com/restaurant_reservation_api/core/reservations/mapper/ReservationsDTOMapper.java
@@ -0,0 +1,27 @@
+package br.com.restaurant_reservation_api.core.reservations.mapper;
+
+import br.com.restaurant_reservation_api.core.reservations.domain.dto.response.ReservationsDTO;
+import br.com.restaurant_reservation_api.core.reservations.domain.entity.Reservations;
+import br.com.restaurant_reservation_api.core.restauranttables.mapper.RestaurantTablesDTOMapper;
+import br.com.restaurant_reservation_api.core.users.mapper.UsersDTOMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ReservationsDTOMapper {
+
+    private final UsersDTOMapper usersDTOMapper;
+    private final RestaurantTablesDTOMapper restaurantTablesDTOMapper;
+
+    public ReservationsDTO convert(Reservations reservations) {
+        return new ReservationsDTO(
+                reservations.getId(),
+                usersDTOMapper.convert(reservations.getUserId()),
+                restaurantTablesDTOMapper.convert(reservations.getRestaurantTableId()),
+                reservations.getReservationDate(),
+                reservations.getReservationStatus(),
+                reservations.getCreatedAt()
+        );
+    }
+}
